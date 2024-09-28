fa4a18cbc39c9655b418e490a02ad2ebbb241037
thalyshenrique7 <thalyshenriquedev@gmail.com>
2024-09-28 14:16:18 -0300

    - updating spring version; - creating integration test config to mysql database.

---
+++ src/main/java/com/devsnop/cashflowkeeper/config/TestConfig.java
@@ -0,0 +1,7 @@
+package com.devsnop.cashflowkeeper.config;
+
+public class TestConfig {
+
+	public static final int SERVER_PORT = 80;
+	public static final String CONTENT_TYPE_JSON = "application/json";
+}
